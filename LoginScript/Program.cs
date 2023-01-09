ca81ad1943e170122fa3845ff0f51aebcaa63ed6
Amy B <110386059+Ab92d@users.noreply.github.com>
2023-01-09 00:00:22 +0000

    Add project files.

---
+++ LoginScript/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace MyApplication
+{
+    class Program
+    {
+        
+
+        static void Login()
+        {
+            string userName = "amy";
+            string password = "123";
+            Console.WriteLine("Enter username: ");
+            string EnteredUsername = Console.ReadLine();
+            Console.WriteLine("Enter password: ");
+            string EnteredPassword = Console.ReadLine();
+            
+            
+
+            if (EnteredUsername.ToLower().Equals(userName) && EnteredPassword.Equals(password))
+            {
+                Console.WriteLine("Access Granted");
+            }
+            else
+            {
+                Console.WriteLine("Access denied");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Login();
+        }
+    }
+}
